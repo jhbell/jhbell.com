69b03ae2ec67a7115b53e1cd3ff10e1ec993ddd7
Jeff Bell <jhbell@users.noreply.github.com>
2014-09-21 22:37:19 -0500

    First upload of site to git repo

---
+++ blogs/blog1.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h3>My first real blog!</h3>';
+echo '<p>20-9-2014</p>';
+echo '<p>SO here is my first real blog to test to see if it goes above the previous. Not much to say! Hope it works!!!';
+echo '<p>-JB</p>';
+?>
